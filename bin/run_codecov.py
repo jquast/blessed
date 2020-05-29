b49d209fa81f0a22ed17a9921e4d6c3fa51c699d
Jeff Quast <contact@jeffquast.com>
2020-05-29 17:30:16 -0400

    multiplexing lookup datasets

--- bin/run_codecov.py
+++ bin/run_codecov.py
@@ -9,12 +9,6 @@ import tenacity
 tenacity.retry(wait=tenacity.wait_random(min=1, max=5),
                stop=tenacity.stop_after_delay(60))
 def main():
-    """Run codecov up to RETRIES times On the final attempt, let it exit normally."""
-
-    # Make a copy of argv and make sure --required is in it
-    args = sys.argv[1:]
-    if '--required' not in args:
-        args.append('--required')
     codecov.main(*args)
 
 
