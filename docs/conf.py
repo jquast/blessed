9bc89e679ba0d85cf8900f9960d3ad6837d27afd
Avram Lubkin <aviso@rockhopper.net>
2024-11-23 17:06:33 -0500

    Fix Sphinx warnings

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,6 @@ import runpy
 import functools
 
 # 3rd party
-import sphinx_rtd_theme
 import sphinx.environment
 from docutils.utils import get_source_line
 
@@ -155,7 +154,7 @@ html_theme = "sphinx_rtd_theme"
 # html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# html_theme_path = []
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
