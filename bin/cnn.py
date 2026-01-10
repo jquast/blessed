57fd3ee3182ec1eb41e907ed2bdf7e4073eb499d
Jeff Quast <contact@jeffquast.com>
2026-01-09 22:17:35 -0500

    bugfix cnn.py and use color_hex

--- bin/cnn.py
+++ bin/cnn.py
@@ -1,13 +1,8 @@
 """Basic example of hyperlinks -- show CNN news site with clickable URL's."""
-# std imports
-import random
-
 # 3rd party
 import requests
-
-# local
-# 3rd-party
 from bs4 import BeautifulSoup
+
 # local imports
 from blessed import Terminal
 
@@ -19,33 +14,42 @@ def embolden(phrase):
 
 def make_bold(term, text):
     # embolden text
-    return ' '.join(term.bold(phrase) if embolden(phrase) else phrase
+    return ' '.join(term.color_hex('#ecc')(phrase) if embolden(phrase) else phrase
                     for phrase in text.split(' '))
 
 
-def whitespace_only(term, line):
-    # return only left-hand whitespace of `line'.
-    return line[:term.length(line) - term.length(line.lstrip())]
-
-
 def find_articles(soup):
-    return (a_link for a_link in soup.find_all('a') if '/article' in a_link.get('href'))
+    return (a_link for a_link in soup.find_all('a') if a_link.get('href').startswith('/')
+            and a_link.text != 'CNN')
 
 
 def main():
     term = Terminal()
-    cnn_url = 'https://lite.cnn.io'
+    cnn_url = 'https://lite.cnn.com'
     soup = BeautifulSoup(requests.get(cnn_url).content, 'html.parser')
     textwrap_kwargs = {
         'width': term.width - (term.width // 4),
-        'initial_indent': ' ' * (term.width // 6) + '* ',
-        'subsequent_indent': (' ' * (term.width // 6)) + ' ' * 2,
+        'initial_indent': ' * ',
+        'subsequent_indent': '   ',
     }
+    print(term.center('CNN Headlines'))
+    print(term.center('============='))
+    print()
+    line_num = 1
     for a_href in find_articles(soup):
-        url_id = random.randrange(0, 1 << 24)
-        for line in term.wrap(make_bold(term, a_href.text), **textwrap_kwargs):
-            print(whitespace_only(term, line), end='')
-            print(term.link(cnn_url + a_href.get('href'), line.lstrip(), url_id))
+        # create headlines with colors and placeholder for clickable hyperlink
+        ansi_text = make_bold(term, a_href.text.strip() + ' [view]')
+        for line in term.wrap(ansi_text, **textwrap_kwargs):
+            # replace with a clickable hyperlink
+            if '[view]' in line:
+                text = term.bright_black('[view]')
+                url = cnn_url + a_href.get('href')
+                line = line.replace('[view]', term.link(url=url, text=text, url_id=line_num))
+            print(' ' * (term.width//8) + line.rstrip())
+            line_num += 1
+        if line_num > term.height - 6:
+            break
+    print()
 
 
 if __name__ == '__main__':
