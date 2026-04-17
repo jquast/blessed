d1d4c846cbd9321eac3e5b246a345527fd6d5b2b
Jeff Quast <contact@jeffquast.com>
2026-04-17 12:48:42 -0400

    revert this script to origin/master

--- bin/keyboard_simple.py
+++ bin/keyboard_simple.py
@@ -3,11 +3,5 @@ from blessed import Terminal
 
 term = Terminal()
 with term.cbreak():
-    #print('\033=')
     key = term.inkey()
-    extra = term.flushinp()
-    print(f"You pressed: {key!r} ({str(key)!r}) extra={str(extra)!r}")
-    #print('\033>')
-    key = term.inkey()
-    extra = term.flushinp()
-    print(f"You pressed: {key!r} ({str(key)!r}) extra={str(extra)!r}")
+    print(f"You pressed: {key!r}")
